1a2b5d5d55b0efe5575c463c862abd344ac19077
Payam <payam@auditready.com>
2025-04-17 08:42:42 +0200

    prazapril: Fix base URL for dev and production

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/",
+  base: process.env.NODE_ENV === "production" ? "/audit-readiness-hub/" : "/",
   server: {
     host: "::",
     port: 8080,
